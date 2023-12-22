4d8a4a431ec6341c5ef7b39311b6ff02558d36b2
IanLynnEE <kmes50311@gmail.com>
2023-12-23 03:05:26 +0800

    Prepare annotation after augmentation for training.

---
+++ augmentation/finalize_selected_images.py
@@ -0,0 +1,82 @@
+import argparse
+import json
+import os
+import shutil
+
+from PIL import Image
+from tqdm import tqdm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--origin_annotation', type=str, default='dataset/annotations/train.json')
+    parser.add_argument('--origin_data_root', type=str, default='dataset/train')
+    parser.add_argument('-g', '--generated_data_root', type=str, required=True)
+    parser.add_argument('-i', '--selected_data_root', type=str, required=True)
+    parser.add_argument('-o', '--output_dir', type=str, required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    ann = get_annotation_of_selected_images(args.origin_annotation, args.selected_data_root)
+
+    os.makedirs(args.output_dir)
+    os.makedirs(os.path.join(args.output_dir, 'generated'))
+    os.makedirs(os.path.join(args.output_dir, 'original'))
+
+    with open(os.path.join(args.output_dir, 'selected_ann.json'), 'w') as f:
+        json.dump(ann, f, indent=4)
+
+    for row in ann['images']:
+        shutil.copy(
+            os.path.join(args.generated_data_root, row['file_name']),
+            os.path.join(args.output_dir, 'generated', row['file_name'])
+        )
+        shutil.copy(
+            os.path.join(args.origin_data_root, row['o_file_name']),
+            os.path.join(args.output_dir, 'original', row['o_file_name'])
+        )
+    get_fid(args, ann)
+
+
+def get_fid(args: argparse.Namespace, ann: dict):
+    generated_resized_dir = os.path.join(args.output_dir, 'generated_resized')
+    original_resized_dir = os.path.join(args.output_dir, 'original_resized')
+    os.makedirs(generated_resized_dir)
+    os.makedirs(original_resized_dir)
+
+    # Resize image to 512x512.
+    for row in tqdm(ann['images'], total=len(ann['images'])):
+        img = Image.open(os.path.join(args.generated_data_root, row['file_name']))
+        img = img.resize((512, 512))
+        img.save(os.path.join(generated_resized_dir, row['file_name']))
+
+        img = Image.open(os.path.join(args.origin_data_root, row['o_file_name']))
+        img = img.resize((512, 512))
+        img.save(os.path.join(original_resized_dir, row['o_file_name']))
+
+    os.system(f'python -m pytorch_fid --device cuda:0 {generated_resized_dir} {original_resized_dir}')
+
+
+def get_annotation_of_selected_images(ori_ann: str, selected_dir: str):
+    with open(ori_ann, 'r') as f:
+        ann = json.load(f)
+
+    img_id_list = [int(f.split('_')[1].split('.')[0]) for f in os.listdir(selected_dir) if f.endswith('.png')]
+    selected_ann = {
+        'categories': ann['categories'],
+        'images': [row for row in ann['images'] if row['id'] in img_id_list],
+        'annotations': [row for row in ann['annotations'] if row['image_id'] in img_id_list],
+    }
+    img_id_to_cat_id = {row['image_id']: row['category_id'] for row in selected_ann['annotations']}
+
+    for row in selected_ann['images']:
+        row['o_file_name'] = row['file_name']
+        row['file_name'] = f'{img_id_to_cat_id[row["id"]]}_{row["id"]:04d}.png'
+    return selected_ann
+
+
+if __name__ == '__main__':
+    main()
--- augmentation/manually_select_images.py
+++ augmentation/manually_select_images.py
@@ -2,7 +2,6 @@ import argparse
 import json
 import os
 
-import pandas as pd
 import torch
 from torchvision.utils import draw_bounding_boxes
 from torchvision.io import read_image, write_png
@@ -10,31 +9,24 @@ from torchvision.io import read_image, write_png
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--annotation_path', type=str, default='outputs/blip2_post.json')
-    parser.add_argument('--generated_data_root', type=str, default='outputs/generation')
-    parser.add_argument('--output_bbox_root', type=str, default='outputs/bbox')
+    parser.add_argument('-a', '--annotation_path', type=str, default='outputs/blip2_post.json')
+    parser.add_argument('-i', '--data_root', type=str, default='outputs/generation')
+    parser.add_argument('-o', '--output', type=str, default='outputs/bbox')
     args = parser.parse_args()
-    os.makedirs(args.output_bbox_root, exist_ok=True)
+    os.makedirs(args.output, exist_ok=True)
     return args
 
 
 def main():
     args = parse_args()
-    # DEBUG
-    original_data_root = 'dataset/train'
-    copy_original_images_dir = 'outputs/original'
-    os.makedirs(copy_original_images_dir, exist_ok=True)
 
     with open(args.annotation_path, 'r') as f:
         ann_list = json.load(f)
+    assert isinstance(ann_list, list), f'annotation file should be a list, but got {type(ann_list)}'
 
     for _, row in enumerate(ann_list):
-        # DEBUG
-        copied_file_name = os.path.join(copy_original_images_dir, f'{row["category_id"]}_{row["image_id"]:04d}.png')
-        os.system(f'cp {os.path.join(original_data_root, row["file_name"])} {copied_file_name}')
-
         generated_img = read_image(os.path.join(
-            args.generated_data_root,
+            args.data_root,
             f'{row["category_id"]}_{row["image_id"]:04d}.png'
         ))
         _, h, w = generated_img.shape
@@ -46,35 +38,9 @@ def main():
             width=5
         )
         write_png(draw, os.path.join(
-            args.output_bbox_root,
+            args.output,
             f'{row["category_id"]}_{row["image_id"]:04d}.png'
         ))
-
-
-def copy_selected_images_and_annotations(df_selected: pd.DataFrame, args: argparse.Namespace):
-    """Copy selected images and annotations to the output directory.
-
-    Args:
-        df_selected (pd.DataFrame):
-            annotation dataframe with information including selected images and corresponding bboxes
-        args (argparse.Namespace): arguments
-    """
-    output_dir = os.path.join(args.output_dir, 'selected')
-    os.makedirs(output_dir, exist_ok=True)
-
-    # Copy selected images.
-    for img_path in df_selected['file_name'].unique():
-        os.system(f'cp {os.path.join(args.data_root, img_path)} {output_dir}')
-
-    with open(args.annotation_path, 'r') as f:
-        ann_json = json.load(f)
-    selected_ann = {
-        'categories': ann_json['categories'],
-        'images': [img for img in ann_json['images'] if img['id'] in df_selected['image_id'].unique()],
-        'annotations': [ann for ann in ann_json['annotations'] if ann['image_id'] in df_selected['image_id'].unique()],
-    }
-    with open(os.path.join(output_dir, 'selected.json'), 'w') as f:
-        json.dump(selected_ann, f, indent=4)
 
 
 if __name__ == '__main__':
